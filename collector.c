32cef8818115c50ccb077caf72f7231f9417062e
Masih <alirezamasih1383@gmail.com>
2025-05-11 11:53:25 +0330

    Made header file to make main function clean

---
+++ collector.c
@@ -0,0 +1,58 @@
+#include "collector.h"
+
+// Function to read the utmp file and return a structure containing the data and its length
+struct utmp_data *read_file(char *path)
+{
+    // Allocate memory for the utmp_data structure to store the data and its length
+    struct utmp_data *Returning_Data = malloc(sizeof(struct utmp_data));
+    struct utmp *utmp_entry = NULL; // Pointer to hold each individual utmp entry
+    struct utmp *entry_list = NULL; // Array to store all the entries read from the file
+
+    int count = 0; // Keep track of the number of entries
+
+    // Open the utmp file and prepare to read from it
+    utmpname(path);  // Open the wtmp file from /var/log as a read-only file
+                     
+    setutent(); // Start reading the data from the utmp file
+
+    
+    // Loop to read each entry from the file until the end
+    while((utmp_entry = getutent()) != NULL)
+    {
+        // Reallocate memory to accommodate the next entry in the list
+        entry_list = realloc(entry_list, (count + 1) * sizeof(struct utmp));
+        if(entry_list == NULL)
+        {
+            // If realloc fails, free the previously allocated memory and print an error
+            free(entry_list);
+            free(Returning_Data);
+            perror("Memory allocation failed.");
+            endutent();
+            return NULL;
+        }    
+                
+        // Store the current utmp entry into the list
+        entry_list[count++] = *utmp_entry;
+    }
+                
+    // Assign the list of entries and the total number of entries to the structure to be returned
+    Returning_Data->data = entry_list;
+    Returning_Data->length = count;
+            
+    // End the use of the utmp file
+    endutent();
+            
+    // Return the structure containing the list of data and the count of entries
+    return Returning_Data;
+}
+                
+void show_time(time_t *time , char *time_buffer_ptr , size_t size)
+{
+
+                
+    struct tm *time_info = localtime(time);
+    strftime(time_buffer_ptr , size , "%a %b %e %H:%M" , time_info);
+
+ 
+}
+
---
+++ collector.h
@@ -0,0 +1,19 @@
+#include <stdio.h> // For I/O operations (input/output)
+#include <stdlib.h> // For memory management (malloc, realloc, free)
+#include <utmp.h> // For reading utmp files
+#include <time.h> // For making time field human readable
+#include <string.h>
+#include <paths.h>
+
+// Define the structure utmp_data to store the list of entries and its length
+struct utmp_data
+{
+    struct utmp *data; // Array to store the utmp structures
+    int length; // Number of entries in the array
+};
+
+// Function declaration to read the utmp file and return the data in a utmp_data structure
+struct utmp_data *read_file(char *path);
+void show_time(time_t *time , char *time_buffer_ptr , size_t size);
+
+
