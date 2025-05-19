d31353121e39753d7cb057716f1651ebbc83ece6
Masih <alirezamasih1383@gmail.com>
2025-05-19 23:27:17 +0330

    Refactor session time handling and improve user session tracking
    - Modularized time formatting for login and logout.
    - Improved handling of user session states with proper formatting.
    - Fixed memory management by freeing allocated memory.
    - NOTE: Small issue with logout time formatting for tty sessions. Prefer showing 'down' instead of the logout time when the session is down.
    - Memory leak issue will be addressed in future commits.

--- collector.c
+++ collector.c
@@ -37,10 +37,7 @@ struct utmp_data *read_file(char *path)
         }    
                 
         // Store the current utmp entry into the list
-        /*if(utmp_entry->ut_type != 8)
-        {
-            entry_list[count++] = *utmp_entry;
-        }*/
+      
         //show_time(&utmp_entry->tv_sec , time_buffer , sizeof(time_buffer));
         strncpy(utmp_entry_formatted.ut_user , utmp_entry->ut_user , 10); 
         strncpy(utmp_entry_formatted.ut_host , utmp_entry->ut_host , 25); 
@@ -63,63 +60,30 @@ struct utmp_data *read_file(char *path)
     return Returning_Data;
 }
                 
-struct session_time *show_time(time_t login_time , time_t logout_time , time_t login_duration)
+struct session_time *show_time(time_t login_time , time_t logout_time)
 {
     struct session_time *time_data = malloc(sizeof(struct session_time));
     
-    char *login = malloc(18 * sizeof(char));
+    char *login = malloc(19 * sizeof(char));
     char *logout = malloc(30 * sizeof(char));
-    char *login_amount = malloc(30 * sizeof(char));
+    //char *login_amount = malloc(30 * sizeof(char));
 
-
-    void set_login_time()
-    {
-        
-        struct tm *login_tm= localtime(&login_time);
-        strftime(login , 17 , "%a %b %e %H:%M" , login_tm);
-        //time_data->login_time = login;
-        strcpy(time_data->login_time , login);
-
-
-    }        
     
-    
-    //struct tm *time_info = localtime(&time);
-    
-    void set_logout_time()
-    {
-        struct tm *logout_tm = localtime(&logout_time);
-        struct tm *login_d_tm = localtime(&login_duration);
 
-        strftime(logout , 30 , "%H:%M" , logout_tm);
-        strftime(login_amount , 30 ,"(%H:%M)" , login_d_tm);
-
-        /* *time_data = (struct session_time){
-
-        
-            .logout_time = logout ,
-            .login_duration = login_amount ,
-    
-        };*/
-        strcpy(time_data->logout_time , logout);
-        strcpy(time_data->login_duration , login_amount);
-
-    }
     if(logout_time == 0)
     {
-        set_login_time();
-        strcpy(time_data->logout_time , "(still"); 
-        strcpy(time_data->login_duration , "logged in)");
+        set_login_time(login_time , login , time_data);
+        strcpy(time_data->logout_time , ""); 
+        //strcpy(time_data->login_duration , "logged in)");
 
     }
     else
     {
-        set_login_time();
-        set_logout_time();
+        set_login_time(login_time , login , time_data);
+        set_logout_time(logout_time  , logout , time_data);
     }
     free(login);
     free(logout);
-    free(login_amount);
 
     return time_data;
     
@@ -127,3 +91,28 @@ struct session_time *show_time(time_t login_time , time_t logout_time , time_t l
  
 }
 
+void set_login_time(time_t login_time , char *login , struct session_time *time_data)
+{
+    
+    struct tm *login_tm= localtime(&login_time);
+    strftime(login , 18 , "%a %b %e %H:%M" , login_tm);
+    strcpy(time_data->login_time , login);
+
+
+}        
+
+    
+//struct tm *time_info = localtime(&time);
+    
+void set_logout_time(time_t logout_time , char *logout , struct session_time *time_data)
+{
+    struct tm *logout_tm = localtime(&logout_time);
+    //struct tm *login_d_tm = localtime(&login_duration);
+
+    strftime(logout , 28 , "%H:%M" , logout_tm);
+    //strftime(login_amount , 30 ,"(%H:%M)" , login_d_tm);
+
+
+    strcpy(time_data->logout_time , logout);
+        
+}
--- logchester.c
+++ logchester.c
@@ -24,6 +24,8 @@ int main()
     //char time_buffer[17]; 
     short record_type;
     int endCounter = 0;
+    int innerCounter = 0;
+    int repeat = -1;
     uint32_t time_offset;
  
     char record_types[9][14] = {
@@ -38,7 +40,7 @@ int main()
         "DEAD_PROCESS"  // 8
     };
     // Loop to print the information of each entry in the data
-    for(int i = 0; i < entry->length; i++)
+    for(int i = entry->length - 1; i >= 0 ; i--)
     {
 
         
@@ -46,48 +48,82 @@ int main()
 
         record_type = data[i].ut_type;
 
-        if(record_type < 8)
-
-        {
-
-            session_time_info = show_time((time_t)data[i].tv_sec , 0 , 0);
-            printf("%-12s%-12s%-27s%-17s- %-8s %-15s%-30s\n" , 
-                    data[i].ut_user ,
-                    data[i].ut_line ,
-                    data[i].ut_host ,
-                    session_time_info->login_time ,
-                    session_time_info->logout_time,
-                    session_time_info->login_duration ,
-                    record_types[record_type]);               
-                    free(session_time_info);
-        }
-        
-        else if(record_type == 8)
+        if(strcmp(data[i].ut_user , "shutdown") == 0)
         {
             endCounter = 1;
-            while(i - endCounter >= 0 )
+            while((i - endCounter) >= 0 && data[i - endCounter].ut_type != 1)
             {
-                if(strcmp(data[i].ut_line , data[i - endCounter].ut_line) == 0)
+                    
+                if(strcmp(data[i - endCounter].ut_host , "login screen")== 0 && (i - endCounter) != repeat)
                 {
-                    time_offset = data[i].tv_sec - data[i - endCounter].tv_sec;
-                    session_time_info = show_time((time_t)data[i - endCounter].tv_sec , (time_t)data[i].tv_sec , (time_t)time_offset); 
+                    session_time_info = show_time((time_t)data[i - endCounter].tv_sec , 0);
 
-                    break;
+                    printf("%-12s%-12s%-27s%-17s- %-8s%-30s\n", 
+                            data[i - endCounter].ut_user,
+                            data[i - endCounter].ut_line,
+                            data[i - endCounter].ut_host,
+                            session_time_info->login_time,
+                            "Down",
+                            record_types[data[i - endCounter].ut_type]);
+                    repeat = i - endCounter;
+    
+                    free(session_time_info);
                 }
-                else
+                else if(data[i - endCounter].ut_type == 8 )
                 {
-                    endCounter++;
+                    innerCounter = 1;
+                    while((i - endCounter - innerCounter) >= 0 && (i - endCounter - innerCounter) != repeat)
+                    {
+                                
+                    if((strcmp(data[i - endCounter].ut_line , data[i - endCounter - innerCounter].ut_line) == 0 && 
+                        strcmp(data[i - endCounter].ut_host , data[i - endCounter - innerCounter].ut_host) == 0 )
+                      ||
+                        (strcmp(data[i - endCounter].ut_line , data[i - endCounter - innerCounter].ut_line) == 0 && 
+                         strcmp(data[i - endCounter].ut_user , data[i - endCounter - innerCounter].ut_user) == 0))
+                      
+                        {
+                            session_time_info = show_time((time_t)data[i - endCounter - innerCounter].tv_sec , (time_t)data[i - endCounter].tv_sec );
+                            printf("%-12s%-12s%-27s%-17s- %-8s%-30s\n",
+                                data[i - endCounter - innerCounter].ut_user,
+                                data[i - endCounter - innerCounter].ut_line,
+                                data[i - endCounter - innerCounter].ut_host,
+                                session_time_info->login_time,
+                                session_time_info->logout_time,
+                                record_types[data[i - endCounter - innerCounter].ut_type]);
+                            repeat = i - endCounter - innerCounter;
+    
+                                break;
+                         }
+                    
+                        innerCounter++;
+                    }
                 }
+                
+                else if(data[i - endCounter].ut_type == 7 && (i - endCounter) != repeat) 
+                {
+                            session_time_info = show_time((time_t)data[i - endCounter].tv_sec , (time_t)data[i].tv_sec );
+                            printf("%-12s%-12s%-27s%-17s- %-8s%-30s\n",
+                                data[i - endCounter].ut_user,
+                                data[i - endCounter].ut_line,
+                                data[i - endCounter].ut_host,
+                                session_time_info->login_time,
+                                session_time_info->logout_time,
+                                record_types[data[i - endCounter].ut_type]);
+                            repeat = i - endCounter;
+
+
+                }
+
+                
+                endCounter++;
             }
         }
 
-
-
-
+        
         
     }
 
-
+    
 
     // Freeing memory to prevent memory leaks.
     free(entry->data);
